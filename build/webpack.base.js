f660ff541c187e9fa7342a88729b636d915260d3
huangming1994 <1157114498@qq.com>
2017-12-25 15:49:55 +0800

    修改dllplugin配置 && add eslint

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -20,6 +20,12 @@ module.exports = {
         exclude: [/node_modules/]
       },
       {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
+      {
         test: /\.(ico|jpg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
         loader: 'file-loader?limit=8192'
       }
--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -7,28 +7,38 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const cheerio = require('cheerio')
 const dllPlugin = require('./config').dll
 const base = require('./webpack.base')
+const dllPath = path.join(process.cwd(), 'node_modules/react-ele-start-dlls')
 
 base.output.publicPath = '/assets/'
 
 // Plugins Configuration
-base.plugins = [
+base.plugins = handlePlugins().concat([
   new ProgressBarPlugin(),
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify('development')
   }),
   new webpack.HotModuleReplacementPlugin(),
   new webpack.NoEmitOnErrorsPlugin(),
-  new webpack.DllReferencePlugin({
-    context: __dirname,
-    manifest: require('../node_modules/react-ele-start-dlls/vendor-manifest.json')
-  }),
   new HtmlWebpackPlugin({
     templateContent: templateContent(),
     inject: true
   }),
   new openBrowserWebpackPlugin({url: 'http://localhost:9999'})
-]
+])
 
+function handlePlugins() {
+  const manifestPath = path.join(dllPath, 'vendor-manifest.json')
+  if (!fs.existsSync(manifestPath)) {
+    console.error('The DLL manifest is missing. Please run `npm run build:dll`')
+    process.exit(0)
+  }
+  return [
+    new webpack.DllReferencePlugin({
+      context: __dirname,
+      manifest: require(manifestPath)
+    })
+  ]
+}
 
 function templateContent() {
   const html = fs.readFileSync(
